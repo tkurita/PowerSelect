619c63077cc66d98b460f99ec6e7724d7524c194
Tetsuro Kurita <tkurita@mac.com>
2013-07-31 01:54:01 +0000

    ASStudio を外したビルドターゲットを追加

--- main.m
+++ main.m
@@ -1,11 +1,15 @@
 #import <mach-o/dyld.h>
 
+#ifdef USE_ASSTUDIO
 extern void ASKInitialize();
+#endif
+
 extern int NSApplicationMain(int argc, const char *argv[]);
 
 int main(int argc, const char *argv[])
 {
+#ifdef USE_ASSTUDIO
 	ASKInitialize();
-	
+#endif	
 	return NSApplicationMain(argc, argv);
 }
